6fc5e58d71a10d18ae67a065b714d4a7099d62b7
sankethaade <103508470+sankethaade@users.noreply.github.com>
2023-11-25 19:55:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you soon!";
+  question.innerHTML = "Yay, see you soon !";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
